c0305a7dda12ba22b65712ad94768e9fb0707847
Ксения <korzhuk.xeniya@yandex.ru>
2024-08-30 01:22:47 +0300

    case1 done

--- src/main/java/api/ReqresTests.java
+++ src/main/java/api/ReqresTests.java
@@ -9,6 +9,26 @@ import static io.restassured.RestAssured.given;
 
 public class ReqresTests {
     private final static String URL = "https://reqres.in/";
+
+    @Test
+    public void case1_successRegisrationAndWithoutPassword(){
+        Specifications.installSpecification(Specifications.requestSpecifications(URL),
+                Specifications.responseSpecification200());
+        Integer id = 4;
+        String token = "QpwL5tke4Pnpja7X4";
+        String email = "eve.holt@reqres.in";
+        String password = "pistol";
+        Register user = new Register(email, password);
+        SuccessRegister successRegister = given()
+                .body(user)
+                .when()
+                .post("api/register")
+                .then().log().all()
+                .extract().as(SuccessRegister.class);
+
+        Assertions.assertEquals(id,successRegister.getId());
+        Assertions.assertEquals(token,successRegister.getToken());
+    }
     @Test
     public void case2_returnListOfUsersAndEmailEndedReqresIn(){
         Specifications.installSpecification(Specifications.requestSpecifications(URL),
